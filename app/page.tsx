8056eb7ff93746abd600116e78655fcdda697f70
Rohan Bagchi <rohan.bagchi01@gmail.com>
2022-11-23 12:45:00 +0000

    Adding changes for codespace forwarded port

--- app/page.tsx
+++ app/page.tsx
@@ -19,27 +19,32 @@ function makeQueryClient() {
 
 const queryClient = makeQueryClient();
 
+const baseURL = 'https://rohanbagchi-orange-space-train-7w6xrqp7pvfrg-3000.preview.app.github.dev/';
+
 export default function Home() {
   const pokemon = use(
     queryClient<Pokemon[]>(
       "pokemon",
       () =>
-        fetch("http://localhost:3000/api/pokemon").then((res) =>
+        fetch(`${baseURL}/api/pokemon`).then((res) =>
           res.json()
         )
     )
   );
 
+  // return <pre>{JSON.stringify(pokemon, null, 2)}</pre>
+
+
   const [selectedPokemon, setSelectedPokemon] = useState<Pokemon>();
 
   const pokemonDetail = selectedPokemon
     ? use(
-        queryClient(
+        queryClient<Pokemon>(
           ["pokemon", selectedPokemon.id].join("-"),
           () =>
-            fetch(`http://localhost:3000/api/${selectedPokemon.id}`).then(
+            fetch(`${baseURL}/api/${selectedPokemon.id}`).then(
               (res) => res.json()
-            ) as Promise<Pokemon>
+            )
         )
       )
     : null;
